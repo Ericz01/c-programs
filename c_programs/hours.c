9d22946bb0fc4e46e7dfa578b42ffd90132ef1f7
Ericz01 <erickinyua770@gmail.com>
2023-04-22 12:15:49 +0300

    hours update

---
+++ c_programs/hours.c
@@ -0,0 +1,26 @@
+#include "cs50.h"
+#include <stdio.h>
+
+int main()
+{
+    int i, n, A;
+    double T;
+    n = get_int("Enter number of weeks: ");
+    int weeks[n];
+    for (i = 0; i < n; i++)
+    {
+        printf("Weeks[%d]= ", i);
+        scanf("%d", &weeks[i]);
+       
+    }
+   for (i = 0; i< n; i++)
+   {
+        printf("Array: %d\n", weeks[i]);
+   }
+   do
+   {
+    if 
+   } while (/* condition */);
+   
+
+}
